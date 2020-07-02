d4f1f5f554ec45cd451d367cba781e7c943f6932
root <awesomelogix@gmail.com>
2020-07-03 03:14:08 +0530

    sand noods
    k
    
    nood when
    
    nood when
    
    Betrayer Kick
    
    README Update
    
    README Update
    
    $(date)
    
    Mon Jul  6 02:30:30 IST 2020
    
    Mon Jul  6 12:35:52 IST 2020
    
    Tue Jul  7 15:50:23 IST 2020
    
    Tue Jul  7 15:50:56 IST 2020
    
    Tue Jul  7 15:53:36 IST 2020
    
    Tue Jul  7 15:58:11 IST 2020
    
    Tue Jul  7 16:00:48 IST 2020
    
    Tue Jul  7 16:01:01 IST 2020
    
    Tue Jul  7 20:43:11 IST 2020
    
    Update README.md

---
+++ config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import socket
+import os
+config['partyName'] = '<your_name> and Logic Teams' #Add your name here
+config['sessionType'] = 'teams'
+config['maxPartySize'] = 11
+config['playlistCode'] = 69420
+config['telnetPassword'] = ''
+config['statsURL'] = ''
+config['partyIsPublic'] = True
+config['port'] = 43210
+config['show_rank'] = True
+config['show_hp'] = True
+config['show_texts'] = False
+config['modded_powerups'] = True
+config['admin_id'] = '' #Your account_id. This will be used to grant you adminship
+config['host'] = '' #Your Name :)
+
+#Admin_ID will be used to link all your servers to a unified database on my server
+#This will allow multiple servers to use the same stats. Pretty Sick.
